28ee977b9544f2989c3d7d3493e63098c33c83e5
Vinh <thevinhblockchain25@gmail.com>
2026-06-24 16:36:13 +0700

    fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   images: {
     remotePatterns: [
       {
@@ -10,9 +9,6 @@ const nextConfig = {
       },
     ],
   },
-=======
-  /* config options here */
->>>>>>> c0393d970b2f378d18ac59ebb71244b48cdb7332
 };
 
 export default nextConfig;
