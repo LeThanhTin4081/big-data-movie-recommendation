ffbe3eafff57db1e6df4af7f009533ec10936a62
Thanh Tin Le <tinlt4081@ut.edu.vn>
2026-06-27 17:57:23 +0700

    update fix vercel

--- frontend/src/app/api/recommend/route.ts
+++ frontend/src/app/api/recommend/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
-import { connectToDatabase } from "@/lib/mongodb";export const dynamic = "force-dynamic";
+import { connectToDatabase } from "@/lib/mongodb";
+
+export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
   try {
